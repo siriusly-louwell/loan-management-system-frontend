26aa75b1379266e1418a6f4fff2fbf1c48a8d4a8
siriusly-louwell <bernaltelouwell23@gmail.com>
2025-10-04 12:57:20 +0800

    address finally fixed

--- src/services/usecases/application/applyUseCase.js
+++ src/services/usecases/application/applyUseCase.js
@@ -13,12 +13,6 @@ export async function applyUseCase(data) {
 
   const applicant = {
     ...data.formData.applicant,
-    // personal_pres: `${address.lot_num}, ${address.purok} ${address.brgy},  ${address.city} ${address.province}, ${address.region}`,
-    // personal_prev: `${address.prev_lot_num}, ${address.prev_purok} ${address.prev_brgy},  ${address.prev_city} ${address.prev_province}, ${address.prev_region}`,
-    // parent_pres: `${address.p_lot_num}, ${address.p_purok} ${address.p_brgy},  ${address.p_city} ${address.p_province}, ${address.p_region}`,
-    // parent_prev: `${address.p_prev_lot_num}, ${address.p_prev_purok} ${address.p_prev_brgy},  ${address.p_prev_city} ${address.p_prev_province}, ${address.p_prev_region}`,
-    // spouse_pres: `${address.sp_lot_num}, ${address.sp_purok} ${address.sp_brgy},  ${address.sp_city} ${address.sp_province}, ${address.sp_region}`,
-    // spouse_prev: `${address.sp_prev_lot_num}, ${address.sp_prev_purok} ${address.sp_prev_brgy},  ${address.sp_prev_city} ${address.sp_prev_province}, ${address.sp_prev_region}`,
     personal_pres: fullAddress.personal_pres,
     personal_prev: fullAddress.personal_prev,
     parent_pres: fullAddress.parent_pres,
@@ -29,8 +23,6 @@ export async function applyUseCase(data) {
     lat: address.lat,
     lng: address.lng,
   };
-
-  // const applicant = { ...data.formData.applicant, ...data.formData.address };
 
   const response = await applyRepository.apply({
     applicant: applicant,

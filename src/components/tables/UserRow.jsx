6756d5a2540f9dc3b0a20bdd86d6e821354ac3d8
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-11-18 14:03:40 +0800

    fix(staff/history): Anauthorized page error.     - Reused CustomerPaymentHIstoryDetails Component in staff route     - fix back button, from fix nav route to (-1)     - removed unused image in customer list.

--- src/components/tables/UserRow.jsx
+++ src/components/tables/UserRow.jsx
@@ -7,11 +7,11 @@ const UserRow = ({ data, click }) => {
             className="flex justify-between p-4 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer"
         >
             <div className="flex items-center space-x-4">
-                <img
+                {/* <img
                     src={data.pfp}
                     alt="User Profile"
                     className="w-12 h-12 rounded-full object-cover border-2 border-gray-300 dark:border-gray-600 shadow-lg"
-                />
+                /> */}
                 <div>
                     <div className="font-bold text-gray-800 dark:text-white">{data.name}</div>
                     <div className="text-sm text-gray-500 dark:text-gray-400">{data.email}</div>

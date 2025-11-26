6ab6ff3c70f770527501a59aa8f5f7186f298c88
Dust1825 <joesel.duazo@webeelabs.com.mt>
2025-11-26 15:17:13 +0800

    revert to reference #

--- src/components/tables/InvoiceRow.jsx
+++ src/components/tables/InvoiceRow.jsx
@@ -42,7 +42,7 @@ export default function InvoiceRow({ data, click }) {
       </dl>
       <dl className="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
         <dt className="text-base font-medium text-gray-500 dark:text-gray-400">
-          Certificate #:
+          Reference #:
         </dt>
         <dd className="mt-2.5 text-sm font-semibold text-gray-900 dark:text-white">
           {data.cert_num}
